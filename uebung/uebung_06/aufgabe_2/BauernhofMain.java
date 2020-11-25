82c15034da3ec6fae030b3d35b8d2295ca6f8f97
MaxGroetsch@lfst <max-groetsch@gmx.de>
2020-11-25 19:05:37 +0100

    Signed-off-by: MaxGroetsch@lfst <max-groetsch@gmx.de>

---
+++ uebung/uebung_06/aufgabe_2/BauernhofMain.java
@@ -0,0 +1,17 @@
+/*
+Aufgabe 2 Vererbter Bauernhof
+Erstellen Sie eine Klasse Bauernhof mit einer main-Methode. 
+Erstellen Sie die Klassen Kuh, Katze und Schwein. 
+Erstellen Sie jeweils ein Objekt dieser Klassen mit den Namen Jack, Joe und Jeff(Variablenname und ein AttributName ). 
+Erstellen Sie in ihrem Bauernhof eine Methode rufen(...), in der Sie einen entsprechenden Parameterverwenden, 
+so dass Sie alle Objekte an diese Methode übergeben können. Die 
+Methode soll folgendes ausgeben:-für Jack: „muh! Jack!“-für Joe: „miau! Joe!“-für Jeff: „oink oink! Jeff!“.
+Überladen Sie die Methode nicht. Hinweis: Sie können die Aufgabe auf (mindestens) zwei verschiedene Arten lösen.
+*/
+public class bauernhufMain {
+
+   public static void main(String[] args) {
+       System.out.println();
+       
+   }
+}
