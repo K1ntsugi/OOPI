52f53d1a7ee5e499ad03053e4ba2750c101b08bc
Max <max-groetsch@gmx.de>
2020-11-28 18:33:04 +0100

    x

---
+++ vrl/Zusatzaufgaben_Basics/Zusatzaufgaben.java
@@ -0,0 +1,12 @@
+public class Zusatzaufgaben {
+    public static void main(String[] args) {
+        // 1
+        System.out.println("Das sind");
+        System.out.println("Zusatzaufgaben");
+        // 2
+        System.out.print("Das sind");
+        System.out.println(" Zusatzaufgaben");
+        // 3
+        int a = (int) (17/2);
+    }
+}
