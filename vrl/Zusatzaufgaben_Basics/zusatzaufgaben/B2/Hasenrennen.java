8f062bf5f413779866baa226410c5a1785a406a5
MaxGroetsch@lfst <max-groetsch@gmx.de>
2021-01-06 21:04:22 +0100

    Hasenrennen

--- vrl/Zusatzaufgaben_Basics/zusatzaufgaben/B2/Hasenrennen.java
+++ vrl/Zusatzaufgaben_Basics/zusatzaufgaben/B2/Hasenrennen.java
@@ -4,11 +4,19 @@ public class Hasenrennen {
     public static void main(String[] args) {
         float schrittl_Igel = 5.3f;
         float schrittl_Hase = 10.8f;
-        float vorsprung = 270f;
+        float abstand = 270f;
+        float start_Igel = 270f;
+        float start_Hase = 0f;
+        int win = 0;
 
-        while (vorsprung != 0)
-        {
-
+        for (float i = abstand; i > start_Hase; i = start_Igel-start_Hase) {
+            System.out.println("Nach " + win + " Schritten: Hase " + start_Hase + " cm, Igel " + start_Igel + " cm");
+            start_Igel += schrittl_Igel;
+            start_Hase += schrittl_Hase;
+            win++;
         }
+
+        System.out.println("Der Hase hat nach " + win + " Schritten gewonnen.");
+
     }
 }
